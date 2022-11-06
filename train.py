7ba631254bb3a7282d26cfbcf3308aa37a4a8ec4
baloonie <unikool8989@gmail.com>
2022-11-06 07:53:22 +0700

    nice bro

--- train.py
+++ train.py
@@ -78,7 +78,7 @@ def train(hyp, opt, device, tb_writer=None):
     nc = 1 if opt.single_cls else int(data_dict['nc'])  # number of classes
     names = ['item'] if opt.single_cls and len(data_dict['names']) != 1 else data_dict['names']  # class names
     assert len(names) == nc, '%g names found for nc=%g dataset in %s' % (len(names), nc, opt.data)  # check
-
+    print("haha")
     # Model
     pretrained = weights.endswith('.pt')
     if pretrained:
